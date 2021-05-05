68abd3dc667203364022a65eb9baeca99aa1cd28
Irsyad Muhammad Andre <irsyad_vertix@apps.ipb.ac.id>
2021-05-05 16:38:59 +0700

    jetpack pro android dicoding bangkit 2021 submisi 1 versi 3 (tinggal testing aja)

--- app/src/androidTest/java/com/dicoding/bangkit/android/jetpack/showcatalogueapp/ui/home/MainActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/bangkit/android/jetpack/showcatalogueapp/ui/home/MainActivityTest.kt
@@ -31,7 +31,6 @@ class MainActivityTest : TestCase() {
         onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourseMovie.size))
 
-
     }
     @Test
     fun loadModuleMovie() {
